a7a5817bf8e2bf58e4cd3ecfd11e796348b77b94
Joey <j.lai@live.ca>
2015-06-03 23:14:03 -0600

    private messaging

--- nodejs-server/server.js
+++ nodejs-server/server.js
@@ -1,36 +1,49 @@
+var querystring = require('querystring');
 var app = require('express')();
 var server = require('http').Server(app);
 server.listen(1337, function(){
   console.log('listening on :1337')
 });
 
-app.get('/message', function(req,res){
-  res.sendFile(__dirname + '/message.html');
+app.get('/', function(req,res){
+  res.sendFile(__dirname + '/index.html');
 });
 
 var socketIO = require('socket.io')(server);
 
-var clients = new Array()
+var clients = {}
+
+function deleteClient(socketId) {
+    for(var userId in clients) {
+        if(clients.hasOwnProperty(userId) && clients[userId] == socketId) {
+            delete clients[userId];
+            break;
+        }
+    }
+}
 
 socketIO.on('connection', function(socket){
-  var userId = socket.handshake.query.userId
+  var userId = socket.handshake.query.userId;
   console.log("a user connected: " + userId);
-  clients[userId] = socket.id
+  clients[userId] = socket.id;
 
   socket.on('disconnect', function() {
     console.log("a user disconnected");
-    delete clients[clients.indexOf(socket.id)]
-    console.log("number of clients: " + clients.length);
+    deleteClient(socket.id);
+    console.log("clients: " + Object.keys(clients));
   });
 
-  socket.on('broadcast message', function(message) {
-    console.log("socket: " + socket.id + " - " + message)
-    socketIO.emit('message', message)
+  socket.on('broadcast message', function(params) {
+    var data = querystring.parse(params);
+    console.log(data);
+    socketIO.emit('message', data.message);
   });
 
-  socket.on('private message', function(data){
+  socket.on('private message', function(params){
+    var data = querystring.parse(params);
+    console.log(data);
     var destination = clients[data.userId];
-    socketIO.sockets.socket(destination).emit(data.message);
+    socketIO.to(destination).emit('message', data.message);
   });
 
 });
